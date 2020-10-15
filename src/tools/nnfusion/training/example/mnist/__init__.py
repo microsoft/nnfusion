6a041fe6079f6d91aeffe17d51d639542eb202ea
Ziming Miao <mzmssg.love@163.com>
2020-10-15 10:33:04 +0800

    e2e mnist training (#63)

--- src/tools/nnfusion/training/example/mnist/__init__.py
+++
@@ -1,2 +0,0 @@
-import nnf
-import dtypes
---
+++ src/tools/nnfusion/training/example/mnist/utils.py
@@ -0,0 +1,12 @@
+from contextlib import contextmanager
+import os
+
+
+@contextmanager
+def cd(newdir):
+    prevdir = os.getcwd()
+    os.chdir(os.path.expanduser(newdir))
+    try:
+        yield
+    finally:
+        os.chdir(prevdir)

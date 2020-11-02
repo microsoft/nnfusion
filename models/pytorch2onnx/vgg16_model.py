e1fe1f07f71f4eb4f410bc2da480b4786c0eebf2
zimiao <zimiao@microsoft.com>
2020-11-02 10:44:32 +0800

    update pt readme

--- models/pytorch2onnx/vgg16_model.py
+++ models/pytorch2onnx/vgg16_model.py
@@ -8,10 +8,13 @@ import torchvision
 
 
 def main():
+    # define pytorch module
     model = torchvision.models.vgg16()
+    # define the pytorch model input/output description
     input_desc = [IODescription("data", [1, 3, 224, 224], torch.float32)]
     output_desc = [IODescription("logits", [1, 1000], torch.float32)]
     model_desc = ModelDescription(input_desc, output_desc)
+    # save the onnx model somewhere
     freezer = PTFreezer(model, model_desc)
     freezer.execute("./vgg16.onnx")
 

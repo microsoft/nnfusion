f6f2ec22cdf9819b45815566f39f40d17e6605dd
yuxiaoguo <yuxiao.guo@outlook.com>
2021-01-27 16:37:20 +0800

    Deconv component is ready

--- src/nnfusion/frontend/onnx_import/op/shape.cpp
+++ src/nnfusion/frontend/onnx_import/op/shape.cpp
@@ -19,9 +19,7 @@
 //  Licensed under the MIT License. See License.txt in the project root for license information.
 //----------------------------------------------------------------------------------------------
 
-#pragma once
-
-#include "core/node.hpp"
+#include "shape.hpp"
 
 namespace nnfusion
 {
@@ -33,8 +31,18 @@ namespace nnfusion
             {
                 NamedNodeVector TranslateShapeOp(const onnx::NodeProto& node_proto,
                                                  const NodeMap& all_ng_nodes,
-                                                 std::shared_ptr<nnfusion::graph::Graph> m_graph);
-
+                                                 std::shared_ptr<nnfusion::graph::Graph> m_graph)
+                {
+                    auto data = GetInputIndex(all_ng_nodes, node_proto, 0);
+                    auto data_shape = data.get_shape();
+                    auto op = std::make_shared<op::Constant>(
+                        nnfusion::element::i64, Shape{data_shape.size()}, data_shape);
+                    op->set_name(node_proto.output(0));
+                    auto gnode = m_graph->add_node_and_edge(op, nnfusion::graph::GNodeVector{});
+                    NamedNodeVector ret{{node_proto.output(0), gnode}};
+                    return ret;
+                }
+                
             } // namespace set_1
 
         } //namespace op

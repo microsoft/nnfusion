e6719117628e9db47df1c700d21165fb6d2abd81
Lingxiao Ma <xysmlx@users.noreply.github.com>
2021-08-10 19:15:37 +0800

    Automatically optimize ONNX Graph with external tool (#300)
    * Automatically optimize ONNX Graph with external tool
    
    * Change log level from ERROR to WARNING
    
    * delete temp file generated by ort_run_frozen script
    
    * update file check

--- src/nnfusion/frontend/onnx_import/onnx.cpp
+++ src/nnfusion/frontend/onnx_import/onnx.cpp
@@ -51,7 +51,44 @@ namespace nnfusion
             load_onnx_model(const std::string& path,
                             const std::unordered_map<std::string, size_t>& dim_params)
         {
-            std::ifstream ifs{path, std::ios::in | std::ios::binary};
+            NNFUSION_LOG(INFO) << "Optimizing ONNX Graph with External Tool "
+                                  "(models/pytorch2onnx/ort_run_frozen.py)";
+            string optimized_filename = string(tmpnam(nullptr));
+            string m_path = path;
+            string script_path =
+                nnfusion::codegen::get_file_from_templates("onnx/ort_run_frozen.py");
+            string cmd = "python3 " + script_path +
+                         " --graph_optimization_level ORT_ENABLE_BASIC "
+                         "--warmup 1 --iters 0 --provider CPUExecutionProvider --file " +
+                         path + " --optimized_model_filepath " + optimized_filename;
+            if (dim_params.size() > 0)
+            {
+                string dim_params_str = " --symbolic_dims \'{";
+                for (auto& it : dim_params)
+                {
+                    if (dim_params_str != " --symbolic_dims \'{")
+                    {
+                        dim_params_str += ", ";
+                    }
+                    dim_params_str += "\"" + it.first + "\": " + to_string(it.second);
+                }
+                dim_params_str += "}\'";
+                cmd += dim_params_str;
+            }
+            int sys_ret = system(cmd.c_str());
+            std::ifstream opt_fin(optimized_filename.c_str());
+            if (sys_ret == 0 && opt_fin.is_open())
+            {
+                m_path = optimized_filename;
+            }
+            else
+            {
+                NNFUSION_LOG(NNFUSION_WARNING)
+                    << "Failed to optimize ONNX Graph with external tool, please "
+                       "check error messages reported by the tool, fallback";
+            }
+
+            std::ifstream ifs{m_path, std::ios::in | std::ios::binary};
             NNFUSION_CHECK(ifs.is_open()) << "failure opening file:" + path;
             string model_dir = "";
             auto pos = path.rfind("/");
@@ -59,7 +96,15 @@ namespace nnfusion
             {
                 model_dir = path.substr(0, pos);
             }
-            return load_onnx_model(ifs, model_dir, dim_params);
+
+            auto graph = load_onnx_model(ifs, model_dir, dim_params);
+
+            if (opt_fin.is_open())
+            {
+                remove(optimized_filename.c_str());
+            }
+
+            return graph;
         }
     } // namespace frontend
 } // namespace nnfusion

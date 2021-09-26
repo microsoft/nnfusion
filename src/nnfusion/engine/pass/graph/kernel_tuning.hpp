7ca652778edc170f58e00efea37ab791152c4022
zimiao <zimiao@microsoft.com>
2021-09-26 15:11:55 +0800

    fix

--- src/nnfusion/engine/pass/graph/kernel_tuning.hpp
+++ src/nnfusion/engine/pass/graph/kernel_tuning.hpp
@@ -27,7 +27,6 @@ namespace nnfusion
 
             private:
                 std::unordered_set<std::string> BlockList;
-                std::unordered_map<std::string, size_t> translated_irs; // ir->cnt
             };
         }
     }

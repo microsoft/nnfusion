b7a34cc125fac5d1b18b0f4a840bb5985d4dd359
Yi Yi <yiyi@microsoft.com>
2021-10-28 17:08:56 +0800

    add thread pool in header file

--- src/nnfusion/engine/pass/graph/runtime_const_folding_pass.hpp
+++ src/nnfusion/engine/pass/graph/runtime_const_folding_pass.hpp
@@ -3,6 +3,11 @@
 
 #pragma once
 
+#include <thread>
+#include <queue>
+#include <condition_variable>
+#include <future>
+
 #include "graph_pass_base.hpp"
 #include "nnfusion/core/operators/op_define/constant.hpp"
 #include "nnfusion/engine/profiler/profiler.hpp"
@@ -20,6 +25,23 @@ namespace nnfusion
                 int runtime_const_folding_iterate_once(
                     std::shared_ptr<Graph>& graph,
                     std::set<std::shared_ptr<GNode>>& blocklist_nodes);
+                class thread_pool
+                {
+                    using Task = std::function<void()>;
+                    std::vector<std::thread> pool;
+                    std::queue<Task> tasks;
+                    std::mutex m_lock;
+                    std::condition_variable cv_task;
+                    std::atomic<bool> stoped;
+                    std::atomic<int> idlThrNum;
+
+                public:
+                    threadpool(unsigned short size);
+                    ~threadpool();
+                    template<class F, class... Args>
+                    auto commit(F&& f, Args&&... args) -> std::future<decltype(f(args...))>;
+                    int idlCount();
+                }
 
             public:
                 bool run_on_graph(std::shared_ptr<Graph>& graph) override;

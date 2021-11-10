3cb9482de3ef8361e11efbcbe5e718e7decb82c0
yiyione <yiyi@microsoft.com>
2021-11-10 08:11:46 +0000

    add test

--- test/nnfusion/core/graph_pass.cpp
+++ test/nnfusion/core/graph_pass.cpp
@@ -10,6 +10,10 @@
 
 DECLARE_string(fconst_folding_backend);
 
+const shared_ptr<Graph> generate_test_graph()
+{
+}
+
 TEST(nnfusion_core, constant_folding_pass)
 {
     using namespace nnfusion::pass::graph;
@@ -19,7 +23,7 @@ TEST(nnfusion_core, constant_folding_pass)
     std::vector<int> values_a(4, 10);
     std::vector<int> values_b(4, 22);
     std::vector<int> values_c(4, 31);
-    auto graph = make_shared<Graph>("constant_folding_pass");
+    auto graph = make_shared<Graph>("constant_folding_pass_without_output");
 
     Shape shape_a{2, 3};
     auto a = make_shared<op::Constant>(element::i32, shape, values_a);
@@ -49,4 +53,17 @@ TEST(nnfusion_core, constant_folding_pass)
     EXPECT_TRUE(graph->get_nodes().at(0)->is_constant());
     EXPECT_EQ(constant_values.size(), 4);
     EXPECT_EQ(constant_values.at(0), "63");
+
+    auto graph_wo = make_shared<Graph>("constant_folding_pass_with_output");
+    auto a_gnode_wo = graph_wo->add_node_and_edge(a, GNodeVector({}));
+    auto b_gnode_wo = graph_wo->add_node_and_edge(b, GNodeVector({}));
+    auto c_gnode_wo = graph_wo->add_node_and_edge(c, GNodeVector({}));
+    auto op_gnode_wo_1 = graph_wo->add_node_and_edge(op1, {a_gnode_wo, b_gnode_wo});
+    auto op_gnode_wo_2 = graph_wo->add_node_and_edge(op2, {op_gnode_wo_1, c_gnode_wo});
+    graph_wo->set_default_outputs();
+    auto node_size_before_wo = graph_wo->get_node_size();
+    const_folding_optimizer.run_on_graph(graph_wo);
+    auto node_size_after_wo = graph_wo->get_node_size();
+    EXPECT_EQ(node_size_before_wo, 5);
+    EXPECT_EQ(node_size_after_wo, 3);
 }

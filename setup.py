240aeab36c660bb5f31a5900a04514eacf37e35a
Wenxiang Hu <wenxh@microsoft.com>
2021-11-21 20:11:18 +0800

    Support to distribute by wheel; New python entry point;

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
-import sys
+from setuptools.command.install_scripts import install_scripts
+import sys, os, tarfile
 if sys.version_info < (3, ):
     print("Please use Python 3 for nnf python library")
     sys.exit(-1)
@@ -11,6 +12,36 @@ if sys.version_info < python_min_version:
     print("You are using Python {}. Python >={} is required.".format(
         platform.python_version(), python_min_version_str))
     sys.exit(-1)
+
+nnf_bin="build/src/tools/nnfusion/nnfusion"
+nnf_tool="build/src/tools/nnfusion/"
+nnf_resource=os.listdir(nnf_tool)
+nnf_blacklist = ["cmake_install.cmake", "Makefile", "CMakeFiles"]
+nnf_pkg = "src/python/nnfusion/nnfusion.tar.gz"
+
+if not os.path.exists(nnf_bin):
+    print("Pleast build nnfusion in /build folder.")
+    sys.exit(-1)
+
+def make_targz(output_filename, source_dir, ignore_list): 
+    tar = tarfile.open(output_filename,"w:gz")
+    for root, dir, files in os.walk(source_dir):
+        skip = False
+        for word in ignore_list:
+            if word in root:
+                skip = True
+                break
+        if skip:
+            continue
+        
+        for file in files:
+            if file in ignore_list:
+                continue
+            pathfile = os.path.join(root, file)
+            tar.add(pathfile, arcname=pathfile.replace("build/src/tools/nnfusion/", ""))
+    tar.close()
+
+make_targz(nnf_pkg, nnf_tool, nnf_blacklist)
 
 import setuptools
 
@@ -26,6 +57,8 @@ with open("requirements_test.txt") as f:
 with open("VERSION") as f:
     version = f.readline().strip()
 
+print(setuptools.find_packages(where="src/python", exclude=["example"]))
+
 setuptools.setup(
     name="nnfusion",
     version=version,
@@ -37,6 +70,7 @@ setuptools.setup(
     url="https://github.com/microsoft/nnfusion",
     packages=setuptools.find_packages(where="src/python", exclude=["example"]),
     package_dir={"": "src/python"},
+    package_data={"":["nnfusion.tar.gz"]},
     install_requires=install_requires,
     tests_require=tests_require,
     classifiers=[
@@ -45,4 +79,5 @@ setuptools.setup(
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "Programming Language :: Python :: 3 :: Only",
         "License :: OSI Approved :: MIT License",
-    ])
+    ]
+    )
---
+++ src/python/nnfusion/__main__.py
@@ -0,0 +1,24 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+import sys, os, tarfile
+
+this_folder = os.path.split(os.path.realpath(__file__))[0]
+nnf_bin=os.path.join(this_folder, "bin/nnfusion")
+nnf_dir=os.path.join(this_folder, "bin/")
+nnf_pkg = os.path.join(this_folder, "nnfusion.tar.gz")
+
+if __name__ == '__main__':
+    if not os.path.exists(nnf_bin):
+        if os.path.exists(nnf_pkg):
+            tar = tarfile.open(nnf_pkg, 'r:gz')
+            tar.extractall(nnf_dir)
+            tar.close()
+            if not os.path.exists(nnf_bin):
+                print("Corrupted nnfusion.tar.gz: Please reinstall nnfusion python package.")
+                exit(-1)
+        else:
+            print("Corrupted nnfusion.tar.gz: Please reinstall nnfusion python package.")
+            exit(-1)
+    args = " ".join(sys.argv[1:])
+    os.system("%s %s"%(nnf_bin, args))

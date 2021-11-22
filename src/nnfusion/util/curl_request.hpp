90b2bd63b85ca742997c92e3ed0fe274a0f65111
Wenxiang Hu <wenxh@microsoft.com>
2021-11-22 20:22:02 +0800

    Fix code style;

--- src/nnfusion/util/curl_request.hpp
+++ src/nnfusion/util/curl_request.hpp
@@ -20,22 +20,19 @@ namespace nnfusion
             if (!std::regex_search(address, match, pattern))
             {
                 NNFUSION_CHECK(false) << "Invalid address format: " << address
-                                        << "expect: <ip>:<port>";
+                                      << "expect: <ip>:<port>";
             }
         }
 
-        void add_custom_header(const std::string& header)
-        {
-            m_custom_headers.emplace_back(header);
-        }
-
+        void add_custom_header(const std::string& header) { m_custom_headers.emplace_back(header); }
         bool send_request(std::string& buffer)
         {
             if (!m_curl_address.empty())
             {
                 // Set custom headers.
                 std::string headers_str = "";
-                for (auto& header : m_custom_headers) {
+                for (auto& header : m_custom_headers)
+                {
                     headers_str += " -H \'" + header + "\'";
                 }
                 std::string cmd = "curl -X GET" + headers_str + " -i -d -w " + m_curl_address;
@@ -65,10 +62,12 @@ namespace nnfusion
         std::string m_curl_address;
         std::vector<string> m_custom_headers;
 
-        string get_response_body(const std::string response) {
+        string get_response_body(const std::string response)
+        {
             const std::string end_of_header = "Transfer-Encoding: chunked";
             auto found = response.find(end_of_header);
-            if (found != std::string::npos) {
+            if (found != std::string::npos)
+            {
                 return response.substr(found + end_of_header.size() + 1);
             }
             return response;

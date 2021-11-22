3151b030d58932f9329dc7f84346abca8b40bd65
yiyione <yiyi@microsoft.com>
2021-11-22 08:40:27 +0000

    update

--- src/nnfusion/util/curl_request.hpp
+++ src/nnfusion/util/curl_request.hpp
@@ -14,7 +14,6 @@ namespace nnfusion
     {
     public:
         CurlRequest(const std::string& address)
-            : m_chunk(NULL)
         {
             m_curl_address = address;
             std::regex pattern("(\\d{1,3}(\\.\\d{1,3}){3}):(\\d+)");
@@ -38,23 +37,24 @@ namespace nnfusion
                 // Set custom headers.
                 std::string headers_str = "";
                 for (auto& header : m_custom_headers) {
-                    headers_str += " -H \"" + header + "\"";
+                    headers_str += " -H \'" + header + "\'";
                 }
-                std::string cmd = "curl" + headers_str + " -d " + buffer + " -w " m_curl_address + " 2> /dev/null";
+                std::string cmd = "curl -X GET" + headers_str + " -i -d -w " + m_curl_address;
 
-                static char line[80];
+                static char line[4096];
                 std::string response;
                 FILE* fp = popen(cmd.c_str(), "r");
                 while (fgets(line, sizeof(line), fp))
                     response += line;
                 pclose(fp);
 
-                if (!response.empty())
+                if (response.substr(0, 12) != "HTTP/1.1 200")
                 {
                     fprintf(stderr, "curl failed: %s\n", response.c_str());
                 }
                 else
                 {
+                    buffer = get_response_body(response);
                     return true;
                 }
             }
@@ -65,5 +65,14 @@ namespace nnfusion
     private:
         std::string m_curl_address;
         std::vector<string> m_custom_headers;
+
+        string get_response_body(const std::string response) {
+            const std::string end_of_header = "Transfer-Encoding: chunked";
+            auto found = response.find(end_of_header);
+            if (found != std::string::npos) {
+                return response.substr(found + end_of_header.size() + 1);
+            }
+            return response;
+        }
     };
 }

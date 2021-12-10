e9e9bd22e2ab2ad9fb7748e6f114517c902f283b
Shi Yining <1700012728@pku.edu.cn>
2021-12-10 13:40:40 +0000

    Adjust parameter

--- src/nnfusion/engine/pass/graph/blockfusion/blockfusion_optimizer.cpp
+++ src/nnfusion/engine/pass/graph/blockfusion/blockfusion_optimizer.cpp
@@ -201,6 +201,8 @@ bool BlockFusionWavefrontOptimizer::verify_node(size_t node_id,
             return false;
         }
     }
+    if (node->get_op_type() == "GatherV2")
+        return false;
 
     cur_group->nodes.push_back(node_id);
     cur_group->block_kernels.push_back(kernel);

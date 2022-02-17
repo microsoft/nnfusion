84e670f0fc87ce363ddff2970a198a116de1a72f
siahuat0727 <tansiahuat@gmail.com>
2022-02-17 15:23:05 +0800

    No need to extract tarball after installation (#380)
    * No need to extract tarball after installation
    
    * Compatible with user installation

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 from __future__ import print_function
-import sys, os, tarfile
+
+import os
+import platform
+import sys
+import tarfile
+
+import setuptools
+
 if sys.version_info < (3, ):
     print("Please use Python 3 for nnf python library")
     sys.exit(-1)
 
-import platform
+
 python_min_version = (3, 5, 2)
 python_min_version_str = '.'.join(map(str, python_min_version))
 if sys.version_info < python_min_version:
@@ -12,40 +19,38 @@ if sys.version_info < python_min_version:
         platform.python_version(), python_min_version_str))
     sys.exit(-1)
 
-nnf_bin="build/src/tools/nnfusion/nnfusion"
-nnf_tool="build/src/tools/nnfusion/"
-nnf_resource=os.listdir(nnf_tool)
+nnf_bin = "build/src/tools/nnfusion/nnfusion"
+nnf_tool = "build/src/tools/nnfusion/"
 nnf_blacklist = ["cmake_install.cmake", "Makefile", "CMakeFiles"]
-nnf_pkg = "src/python/nnfusion/nnfusion.tar.gz"
 
 if not os.path.exists(nnf_bin):
     print("Pleast build nnfusion in /build folder.")
     sys.exit(-1)
 
-def make_targz(output_filename, source_dir, ignore_list): 
-    tar = tarfile.open(output_filename,"w:gz")
-    for root, dir, files in os.walk(source_dir):
-        skip = False
-        for word in ignore_list:
-            if word in root:
-                skip = True
-                break
-        if skip:
-            continue
-        
-        for file in files:
-            if file in ignore_list:
-                continue
-            pathfile = os.path.join(root, file)
-            tar.add(pathfile, arcname=pathfile.replace("build/src/tools/nnfusion/", ""))
-    tar.close()
 
-if os.path.exists(nnf_pkg):
-    os.remove(nnf_pkg)
+def get_data_files(source_dir, ignore_list):
+    def dir_files_pair(root, files):
+        directory = os.path.relpath(root, source_dir)
+        directory = os.path.join('nnfusion-bin', directory)
+        files = [
+            os.path.join(root, file)
+            for file in files
+            if file not in ignore_list
+        ]
+        return directory, files
 
-make_targz(nnf_pkg, nnf_tool, nnf_blacklist)
+    pairs = [
+        dir_files_pair(root, files)
+        for root, dir, files in os.walk(source_dir)
+        if not any(word in root for word in ignore_list)
+    ]
+    # Remove if files is empty
+    return [
+        pair
+        for pair in pairs
+        if pair[1]
+    ]
 
-import setuptools
 
 with open("README.md", encoding="utf-8") as f:
     long_description = f.read()
@@ -59,8 +64,6 @@ with open("requirements_test.txt") as f:
 with open("VERSION") as f:
     version = f.readline().strip()
 
-print(setuptools.find_packages(where="src/python", exclude=["example"]))
-
 setuptools.setup(
     name="nnfusion",
     version=version,
@@ -72,7 +75,7 @@ setuptools.setup(
     url="https://github.com/microsoft/nnfusion",
     packages=setuptools.find_packages(where="src/python", exclude=["example"]),
     package_dir={"": "src/python"},
-    package_data={"":["nnfusion.tar.gz"]},
+    data_files=get_data_files(nnf_tool, nnf_blacklist),
     install_requires=install_requires,
     tests_require=tests_require,
     classifiers=[
@@ -87,4 +90,4 @@ setuptools.setup(
             'nnfusion=nnfusion.__main__:main',
         ],
     }
-    )
+)
--- src/python/nnfusion/__main__.py
+++ src/python/nnfusion/__main__.py
@@ -1,46 +1,25 @@
 # Copyright (c) Microsoft Corporation.
 # Licensed under the MIT License.
 
-import sys, os, tarfile, shutil, logging
-from typing_extensions import runtime
+import logging
+import os
+import site
+import sys
 
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
-this_folder = os.path.split(os.path.realpath(__file__))[0]
-nnf_bin=os.path.join(this_folder, "bin/nnfusion")
-nnf_dir=os.path.join(this_folder, "bin/")
-nnf_pkg = os.path.join(this_folder, "nnfusion.tar.gz")
 
-def check_pkg():
-    if os.path.exists(nnf_bin) and os.path.exists(nnf_pkg):
-        old_time = os.path.getctime(nnf_bin)
-        new_time = os.path.getctime(nnf_pkg) 
-        if new_time > old_time:
-            logging.info("Replacing old nnfusion cli.")
-            shutil.rmtree(nnf_dir)
-    else:
-        if os.path.exists(nnf_bin):
-            logging.info("No nnfusion cli found: Try to extract it from nnfusion.tar.gz.")
-
-
-def extract_pkg():
-    if os.path.exists(nnf_pkg):
-        tar = tarfile.open(nnf_pkg, 'r:gz')
-        tar.extractall(nnf_dir)
-        tar.close()
-        if not os.path.exists(nnf_bin):
-            logging.error("Corrupted nnfusion.tar.gz: Please reinstall nnfusion python package.")
-            exit(-1)
-    else:
-        logging.error("Missing nnfusion.tar.gz: Please reinstall nnfusion python package.")
-        exit(-1)
 
 def run_cli():
-    if os.path.exists(nnf_bin):
-        args = " ".join(sys.argv[1:])
-        os.system("%s %s"%(nnf_bin, args))
-    else:
+    nnf_bin = os.path.join(site.USER_BASE, "nnfusion-bin/nnfusion")
+    if not os.path.exists(nnf_bin):
+        nnf_bin = os.path.join(sys.prefix, "nnfusion-bin/nnfusion")
+    if not os.path.exists(nnf_bin):
         logging.error("No nnfusion cli found: Try to reinstall nnfusion.")
-        exit(-1)
+        sys.exit(-1)
+
+    args = " ".join(sys.argv[1:])
+    os.system("%s %s" % (nnf_bin, args))
+
 
 def welcome():
     print("     _  __ _  __ ____            _ ")
@@ -49,13 +28,11 @@ def welcome():
     print("  /_/|_//_/|_//_/   \\_,_//___//_/ \\___//_//_/")
     print("      MSRAsia NNFusion Team(@nnfusion)")
     print("    https://github.com/microsoft/nnfusion")
-    print("") 
+    print("")
 
 
 def main():
     welcome()
-    check_pkg()
-    extract_pkg()
     run_cli()
 
 

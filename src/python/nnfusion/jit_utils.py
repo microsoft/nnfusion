c4e2c1732847c0c1bb49fdb8e92138306e8a7a7f
siahuat0727 <tansiahuat@gmail.com>
2022-03-22 09:45:21 +0000

    Change relative path to __module__

--- src/python/nnfusion/jit_utils.py
+++ src/python/nnfusion/jit_utils.py
@@ -15,11 +15,9 @@ class TorchModule(torch.nn.Module):
         return self.func(*args, **kwargs)
 
 
-def get_signature(obj, suffix=''):
+def get_signature(obj):
     """
-    Signature of a function or torch.nn.Module instance to detect reusable
-    kernel.
-    For details, please refer to https://github.com/microsoft/nnfusion/pull/379
+    Signature of an object to detect reusable kernel.
     """
 
     if isinstance(obj, torch.nn.Module):
@@ -32,15 +30,7 @@ def get_signature(obj, suffix=''):
     ):
         raise Exception(f"Not support type {obj} for get_signature")
 
-    def get_qualname():
-        name = obj.__qualname__
-        # Remove special chars to avoid the trouble of dealing with paths
-        return re.sub("[<>]", "", name)
+    signature = "-".join([obj.__module__, obj.__qualname__])
 
-    def get_path():
-        # Avoid collision between different files
-        obj_path = inspect.getsourcefile(obj)
-        relpath = os.path.relpath(obj_path)
-        return "-".join(Path(os.path.splitext(relpath)[0]).parts)
-
-    return "-".join(('nnf', get_path(), get_qualname())) + suffix
+    # Remove special chars to avoid the trouble of dealing with paths
+    return re.sub("[<>]", "", signature)

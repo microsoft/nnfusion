0c68b0bc5b876f67da3c2c885e62e072d8bf1abf
Shi Yining <1700012728@pku.edu.cn>
2022-03-28 10:25:54 +0800

    fix debug console

--- python/memopt/debug.py
+++ python/memopt/debug.py
@@ -4,10 +4,9 @@ import code, rlcompleter, readline
 This function can start a interactive debug console
 example:
 from .debug import debug
-debug(globals().update(locals()))
+debug({**globals(), **locals()})
 """
 def debug(vars):
     readline.set_completer(rlcompleter.Completer(vars).complete)
     readline.parse_and_bind("tab: complete")
-    print(globals())
     code.InteractiveConsole(vars).interact()

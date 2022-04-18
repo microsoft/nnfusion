5ffbfb982cd68008d57d8fcd2e1208285a4031bb
Shanbin Ke <shanbinke@gmail.com>
2022-04-18 04:59:02 -0700

    add missing files (#422)

---
+++ artifacts/microbenchmark/tvm/script/tensor_core/bench0_matmul_tensor_core.sh
@@ -0,0 +1,4 @@
+python3 $1/matmul_tuning.py 65536 1024 4096 $2
+python3 $1/matmul_tuning.py 65536 1024 1024 $2
+python3 $1/matmul_tuning.py 65536 4096 1024 $2
+python3 $1/matmul_tuning.py 65536 16384 1024 $2

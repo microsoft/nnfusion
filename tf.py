74f0578db729cb72c7dbe6d1b2336e096c8c6904
Shi Yining <1700012728@pku.edu.cn>
2022-07-05 05:47:41 +0000

    Drop unused files

--- tf.py
+++
@@ -1,9 +0,0 @@
-import tensorflow as tf
-import numpy as np
-
-with tf.device('/gpu:0'):
-    x = tf.placeholder(dtype=tf.float32, shape=(None, 3, 224, 224), name='x')
-    layer = tf.keras.layers.LayerNormalization(axis=-1)
-    y = layer(x)
-    with tf.Session() as sess:
-        a = sess.run([y], feed_dict={x: np.zeros([64, 3, 224, 224])})

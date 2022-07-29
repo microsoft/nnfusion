8e5f271525d080be80fca3cb8733bb1e1389b6ff
zyeric <619828575@qq.com>
2022-07-29 15:12:20 +0800

    refine apis code

--- artifacts/roller/apis.py
+++ artifacts/roller/apis.py
@@ -1,15 +1,14 @@
-from roller.op import Op, MatmulOp
+from roller.op import Op
 from roller.policy import *
-# from roller.test_config import matmul_expr
-from roller.arch import V100
+from roller.arch import dispatch_arch
 from roller.codegen.op_impl.codegenR import CodeGeneratorR
 
 def get_config_space(op, device_name):
     print('[debug] devname =', device_name)
 
-    roller_op = Op(op, (4096, 4096, 4096), 'float32', False)
-    # TODO
-    roller_arch = V100()
+    shape = [item.dom.extent.value for item in op.axis] + [item.dom.extent.value for item in op.reduce_axis]
+    roller_op = Op(op, shape, op.output(0).dtype, False)
+    roller_arch = dispatch_arch(device_name)
 
     # print(roller_op.IODependent())
 
@@ -18,42 +17,15 @@ def get_config_space(op, device_name):
     else:
         policy = ConstructionPolicyPlainRT(roller_op, roller_arch, True, True, True, 1.0)
 
-    # TODO: Replace following with Roller's config list produced by rProg
-    # candidates = [{"N": 1, "M": 2}, {"N": 2, "M": 1}]
-    # return candidates
-
     rprogs = policy.emit_config_without_trails(10)
-    return rprogs
+    candidates = [(rprog, policy.get_align_info(rprog, roller_arch, True, True, target_stage=op.output(0).name, st_align=False), roller_arch) for rprog in rprogs]
+    return candidates
 
 def apply_config(op, sched, config):
-    # import tvm
-    print('[debug] config =', config.Dump())
-    # print(tvm.lower(sched, op.input_tensors, simple_mode=True))
-    # TODO: Replace following with Roller's rewrite_schedule()
-    # sched[op].bind(op.axis[0], tvm.te.thread_axis('blockIdx.x'))
+    rprog, align_info, roller_arch = config
+    print('[debug] config =', rprog.Dump())
 
-    roller_op = Op(op, (4096, 4096, 4096), 'float32', False)
-    roller_arch = V100()
-
-    if roller_op.IODependent():
-        policy = ConstructionPolicyRT(roller_op, roller_arch, True, True, False, 1.0, True)
-    else:
-        policy = ConstructionPolicyPlainRT(roller_op, roller_arch, True, True, True, 1.0)
-
-    align_info = policy.get_align_info(config, roller_arch, True, True, target_stage=op.output(0).name, st_align=False)
-
-    # print(align_info)
-    # print(op.output(0).name)
     cgen = CodeGeneratorR()
-    # s = tvm.te.create_schedule(op)
-    # print(tvm.lower(sched, op.input_tensors, simple_mode=True))
-    cgen.rewrite_schedule(sched, config, True, True, target_stage=op.output(0).name, align_info=align_info, bank_size=roller_arch.smem_bank_size)
-    # print(tvm.lower(sched, op.input_tensors, simple_mode=True))
-
-    # in_tensors = op.input_tensors
-    # out_tensor = op.output(0)
-    # print([in_tensors[0], in_tensors[1]] + [out_tensor])
-    # func = tvm.build(sched, [in_tensors[0], in_tensors[1]] + [out_tensor], 'cuda', name='template_op')
-    # print(func.imported_modules[0].get_source())
+    cgen.rewrite_schedule(sched, rprog, True, True, target_stage=op.output(0).name, align_info=align_info, bank_size=roller_arch.smem_bank_size)
 
     return sched
--- artifacts/roller/arch/__init__.py
+++ artifacts/roller/arch/__init__.py
@@ -2,3 +2,15 @@ from .K80 import *
 from .IPU import *
 from .V100 import *
 from .MI50 import *
+
+def dispatch_arch(device_name):
+    if device_name == 'K80':
+        return K80()
+    elif device_name == 'IPU':
+        return IPU()
+    elif device_name == 'V100':
+        return V100()
+    elif device_name == 'MI50':
+        return MI50()
+    else:
+        assert(False)

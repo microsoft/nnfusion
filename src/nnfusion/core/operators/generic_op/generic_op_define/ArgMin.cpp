2c4f4afd8bb6434b089c72edd3032448e11f46b3
LeiWang1999 <2230647190@qq.com>
2022-11-15 15:02:48 +0000

    fix some op

--- src/nnfusion/core/operators/generic_op/generic_op_define/ArgMin.cpp
+++ src/nnfusion/core/operators/generic_op/generic_op_define/ArgMin.cpp
@@ -41,7 +41,14 @@ REGISTER_OP(ArgMin)
                 NNFUSION_CHECK_FAIL() << "ArgMax: axis out of range";
 
             auto output_shape = curr->get_output_shape(0);
-            auto temp_shape = curr->get_output_element_type(0);
+            auto output_datatype = curr->get_output_element_type(0);
+            NNFUSION_CHECK(output_datatype == nnfusion::element::i64 ||
+                           output_datatype == nnfusion::element::i32 ||
+                           output_datatype == nnfusion::element::i16)
+                << "ArgMin: output datatype must be i64, i32 or i16";
+            auto output_datatype_str = output_datatype.c_type_string();
+            // remove _t in output_datatype_str, example: int64_t to int64
+            output_datatype_str = output_datatype_str.substr(0, output_datatype_str.size() - 2);
             // make layout from shape
             std::set<int> reduced_axis;
             for (int i = 0; i < input_shape.size(); ++i)
@@ -54,12 +61,13 @@ REGISTER_OP(ArgMin)
             auto input0_layout =
                 vector_to_string<std::vector<std::string>> (op::create_layout_from_dims(input_shape));
             auto ir_template =
-                R"( mediate0@mediate0_layout@ <=! @input0@@input0_layout@;@output0@@output0_layout@ <=! @index0@.when([mediate0@mediate0_layout@ == @input0@@input0_layout@], @index0@.val()); )";
+                R"( mediate0@mediate0_layout@ <=! @input0@@input0_layout@;@output0@@output0_layout@ <=! @index0@.when([mediate0@mediate0_layout@ == @input0@@input0_layout@], @index0@.val()).cast(`@output_datatype@`); )";
             op::OpConfig::any op_config;
             op_config["input0_layout"] = input0_layout;
             op_config["mediate0_layout"] = mediate0_layout;
             op_config["index0"] = axis ? "N1" : "N0";
             op_config["output0_layout"] = output0_layout;
+            op_config["output_datatype"] = output_datatype_str;
             auto expression = op::create_code_from_template(ir_template, op_config);
             NNFUSION_LOG(INFO) << expression;
             return expression;

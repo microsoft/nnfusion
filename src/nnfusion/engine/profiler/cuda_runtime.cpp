82471449d461627c494eaa2f643d76f7cd72b15a
Jilong Xue <jxue@microsoft.com>
2022-11-24 20:57:15 +0800

    revoke long long to int64_t replace

--- src/nnfusion/engine/profiler/cuda_runtime.cpp
+++ src/nnfusion/engine/profiler/cuda_runtime.cpp
@@ -7,7 +7,6 @@
  */
 
 #include "cuda_runtime.hpp"
-#include <regex>
 #include "nnfusion/core/kernels/cuda_gpu/cuda_emitter.hpp"
 #include "nnfusion/core/kernels/cuda_gpu/cuda_langunit.hpp"
 #include "nnfusion/engine/async_manager.hpp"
@@ -106,10 +105,7 @@ bool CudaDefaultRuntime::codegen(const ProfilingContext::Pointer& ke)
 
     // Write function definition
     writer << fu->comment_unit->get_code();
-    std::string sig_str = fu->get_specialized_signature().c_str();
-    sig_str = std::regex_replace(sig_str, std::regex("long long"), "int64_t");
-    writer << sig_str << "\n";
-    //writer << fu->get_specialized_signature() << "\n";
+    writer << fu->get_specialized_signature() << "\n";
     writer.block_begin();
     writer << body_unit << "\n";
     writer.block_end();

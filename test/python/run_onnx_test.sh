1cf82052c9217eb39abe6e7bb402318366762052
github <notifications@github.com>
2022-11-29 12:49:01 +0800

    Redir folder;

--- test/python/run_onnx_test.sh
+++ test/python/run_onnx_test.sh
@@ -1,5 +1,5 @@
-mkdir -p ground_truth/
-python test/python/run_onnx_test.py -f test/python/default_operators.txt -o default.csv
-python test/python/run_onnx_test.py -f test/python/default_operators.txt -i -o input_as_constant.csv
-python test/python/run_onnx_test.py -f test/python/default_operators.txt -i -a -o float16.csv
-python test/python/run_onnx_test.py -f test/python/default_operators.txt -i -d -o float64.csv
+mkdir -p test_snapshot/
+python test/python/run_onnx_test.py -f test/python/default_operators.txt -o test_snapshot/default.csv
+python test/python/run_onnx_test.py -f test/python/default_operators.txt -i -o test_snapshot/input_as_constant.csv
+python test/python/run_onnx_test.py -f test/python/default_operators.txt -i -a -o test_snapshot/float16.csv
+python test/python/run_onnx_test.py -f test/python/default_operators.txt -i -d -o test_snapshot/float64.csv

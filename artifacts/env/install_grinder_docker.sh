16713b3cdb5f78e6d38fdae06b604efd08d5b851
heheda <zhangch99@outlook.com>
2023-04-19 19:51:39 +0800

    install_grinder script

--- artifacts/env/install_grinder_docker.sh
+++ artifacts/env/install_grinder_docker.sh
@@ -3,6 +3,7 @@ cd /root/nnfusion
 mkdir build && cd build && cmake .. && make -j
 
 cd /root/nnfusion/artifacts
+source /root/miniconda3/etc/profile.d/conda.sh
 conda activate grinder
 pip install -e .
 conda deactivate

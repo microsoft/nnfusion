510a5927454dea12cb346816e4db29322b385f39
LeiWang1999 <leiwang1999@outlook.com>
2023-07-09 01:09:48 -0800

    update quatization scrips

--- quantization/quantize_gptq.py
+++ quantization/quantize_gptq.py
@@ -1,21 +1,27 @@
-import time
-import torch
 import os
 import sys
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-from auto_gptq import AutoGPTQForCausalLM, BaseQuantizeConfig
 from transformers import AutoTokenizer, TextGenerationPipeline, AutoModelForCausalLM
-
+from auto_gptq import AutoGPTQForCausalLM, BaseQuantizeConfig
+import torch
+import time
 
 enable_quantize = True
-export_nnfusion = False
-use_triton = True
+export_nnfusion = True
+use_triton = False
 
 assert not (export_nnfusion and use_triton)
 
-pretrained_model_dir = "/workspace/v-leiwang3/lowbit_model/oa_llama_30b/oasst-rlhf-2-llama-30b-7k-steps-xor"
-quantized_model_dir = f"quantization/models/huggingchat-30b-rlhf-2-4bit{'-triton' if use_triton else ''}"
+import argparse
+parser = argparse.ArgumentParser()
+# add pretrained_model_dir
+parser.add_argument("--pretrained_model_dir", type=str, default=None)
+parser.add_argument("--quantized_model_dir", type=str, default=None)
+
+args = parser.parse_args()
+pretrained_model_dir = args.pretrained_model_dir
+quantized_model_dir = args.quantized_model_dir
 
 tokenizer = AutoTokenizer.from_pretrained(pretrained_model_dir, use_fast=True)
 
@@ -34,29 +40,28 @@ if enable_quantize:
     )
 
     # load un-quantized model, the model will always be force loaded into cpu
-    model = AutoGPTQForCausalLM.from_pretrained(
-        pretrained_model_dir, quantize_config)
+    model = AutoGPTQForCausalLM.from_pretrained(pretrained_model_dir, quantize_config)
 
-    # quantize model, the examples should be list of dict whose keys can only be "input_ids" and "attention_mask"
+    # quantize model, the examples should be list of dict whose keys can only be "input_ids" and "attention_mask" 
     # with value under torch.LongTensor type.
-    model.quantize(examples, use_tvm=False if use_triton else True,
-                   use_triton=use_triton, export_nnfusion=export_nnfusion, autotune_warmup_after_quantized=True)
+    model.quantize(examples, use_tvm=False if use_triton else True, use_triton=use_triton, export_nnfusion=export_nnfusion)
 
     # save quantized model
     model.save_quantized(quantized_model_dir)
 # load quantized model to the first GPU
-model = AutoGPTQForCausalLM.from_quantized(quantized_model_dir, device="cuda:0", use_tvm=False if use_triton else True,
-                                           use_triton=use_triton, export_nnfusion=export_nnfusion).half().cuda()
+model = AutoGPTQForCausalLM.from_quantized(quantized_model_dir, device="cuda:0", use_tvm=False if use_triton else True, use_triton=use_triton, export_nnfusion=export_nnfusion).half().cuda()
 # or you can also use pipeline
 pipeline = TextGenerationPipeline(model=model, tokenizer=tokenizer)
 print(pipeline("auto-gptq is")[0]["generated_text"])
 
 # export 2 onnx
-batch_size = 128
+batch_size = 1
 seq_length = 1
 input_shape = (batch_size, seq_length)
 onnx_name = f"qmodel_b{batch_size}s{seq_length}.onnx"
-output_path = os.path.join(quantized_model_dir, onnx_name)
+output_path = os.path.join(quantized_model_dir, f"qmodel_b{batch_size}s{seq_length}", onnx_name)
+if not os.path.exists(os.path.dirname(output_path)):
+    os.makedirs(os.path.dirname(output_path))
 input_ids = torch.ones(input_shape, dtype=torch.long, device="cuda:0")
 attention_mask = torch.ones(input_shape, dtype=torch.long, device="cuda:0")
 
@@ -68,20 +73,17 @@ if not export_nnfusion:
     print("time: ", (end - start) / 100 * 1000, "ms")
     print(outputs.logits)
 else:
+    model = model.half().cuda()
+    torch.onnx.export(      
+        model,  
+        input_ids,  
+        f=output_path,  
+        opset_version=11, 
+    )     
     import onnx
     from onnxsim import simplify
-    model = model.half().cuda()
-    torch.onnx.export(
-        model,
-        input_ids,
-        f=output_path,
-        opset_version=11,
-    )
-    # load your predefined ONNX model
     model = onnx.load(output_path)
-    # convert model
     model_simp, check = simplify(model)
-    sim_output_path = os.path.join(
-        quantized_model_dir, f"qmodel_b{batch_size}s{seq_length}_sim.onnx")
-    onnx.save(model_simp, sim_output_path,
-              save_as_external_data=True, all_tensors_to_one_file=False)
+    assert check, "Simplified ONNX model could not be validated"
+    onnx.save(model_simp, output_path, save_as_external_data=True)
+    print("export onnx done")
